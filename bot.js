0f484f654487464b372d3555d204ae9c6dc15375
gitacitrap <puspita.gitacitra@gmail.com>
2018-05-30 04:23:03 +0700

    tes bot

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'TOKENTELEGRAM';
+const token = '508931900:AAGp4iKP1DTqdvBjBTqq7C8pU8UKS43ml50';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -17,7 +17,7 @@ bot.on('message', (msg) => {
 	// bot.sendMessage(chatId,worng_salam.test(message));
 
 	//logic untuk salam
-	worng_salam.test(message)?bot.sendMessage(chatId, "Muslim yang baik harus mengucapkan salam dengan lengkap.\nSeperti \"Assalamu'alaikum\" atau \"Assalamu'alaikum Warahmatullahi Wabarakatuh\".\nCoba tolong di ucap lagi salamnya.\n"): good_salam.test(message)? bot.sendMessage(chatId, "Wa'alaikumussalam Warahmatullahi Wabarakatuh"):message.replace(ask,'').length>0?message.replace(ask,'').includes("kabar")?bot.sendMessage(chatId,"Alhamdulillah, baik..."): message.replace(ask,'').match(/(nama*)+(kamu)?/)?bot.sendMessage(chatId,"Muhammad Oemar :)"): message.replace(ask,'').match(/(tuhan*)+(kamu)?/)?bot.sendMessage(chatId,"Allah SWT :)"):message.replace(ask,'').match(/(rasul*)+(kamu)?/)?bot.sendMessage(chatId,"Nabi Muhammad SAW :)") : bot.sendMessage(chatId,":)"):bot.sendMessage(chatId,"Hmm...\nAku tidak mengerti pertanyaanmu.");
+	worng_salam.test(message)?bot.sendMessage(chatId, "Muslim yang baik harus mengucapkan salam dengan lengkap.\nSeperti \"Assalamu'alaikum\" atau \"Assalamu'alaikum Warahmatullahi Wabarakatuh\".\nCoba tolong di ucap lagi salamnya.\n"): good_salam.test(message)? bot.sendMessage(chatId, "Wa'alaikumussalam Warahmatullahi Wabarakatuh"):message.replace(ask,'').length>0?message.replace(ask,'').includes("kabar")?bot.sendMessage(chatId,"Alhamdulillah, baik..."): message.replace(ask,'').match(/(nama*)+(kamu)?/)?bot.sendMessage(chatId,"Muhammad Oemar :)"): message.replace(ask,'').match(/(tuhan*)+(kamu)?/)?bot.sendMessage(chatId,"Allah SWT :)"):message.replace(ask,'').match(/(rasul*)+(kamu)?/)?bot.sendMessage(chatId,"Nabi Muhammad SAW :)") : bot.sendMessage(chatId,"Jazakillahu khoir, ukhti :)"):bot.sendMessage(chatId,"Hmm...\nAku tidak mengerti pertanyaanmu.");
 	// bot.sendMessage(chatId,ask.test(message));
 
 });
